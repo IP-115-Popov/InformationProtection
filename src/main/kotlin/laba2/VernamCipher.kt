4d45a213f5e6cf339a0932aa21236425c98457c6
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-29 09:55:44 +0700

    Добавил в VernamCipher передачу данных через файл

--- src/main/kotlin/laba2/VernamCipher.kt
+++ src/main/kotlin/laba2/VernamCipher.kt
@@ -7,12 +7,18 @@ object VernamCipher {
         val inputFilePath = "C:/Users/serzh/IdeaProjects/InformationProtection/src/main/kotlin/laba2/i1.jpg"
         val outputFilePath = "C:/Users/serzh/IdeaProjects/InformationProtection/src/main/kotlin/laba2/i2.jpg"
 
+        val outputFilePathE = "C:/Users/serzh/IdeaProjects/InformationProtection/src/main/kotlin/laba2/e2.jpg"
+
 
         val key = Random.nextInt(0, 1_000_000_000)
         // Преобразование JPG в массив Int
         val intArray = JpegToIntArrayConverter.jpegToIntArray(inputFilePath)
+//кодирование
+        val codeArrE = intArray.map{CecodingDececoding(key, it)}.toIntArray()
+        JpegToIntArrayConverter.intArrayToJpeg(codeArrE, outputFilePathE)
 
-        val codeArr = intArray.map{CecodingDececoding(key, it)}
+        //декодинг
+        val codeArr = JpegToIntArrayConverter.jpegToIntArray(outputFilePathE)
 
         val decode : IntArray = codeArr.map { CecodingDececoding(key,it)}.toIntArray()
         // Преобразование массива Int обратно в JPG
