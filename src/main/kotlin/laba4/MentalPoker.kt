b37c3ea3cc4b9a5644e272dd602419092549ed5e
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-11-24 19:56:55 +0700

    (Lab3) MentalPoker

--- src/main/kotlin/laba4/MentalPoker.kt
+++ src/main/kotlin/laba4/MentalPoker.kt
@@ -64,37 +64,46 @@ object MentalPoker {
         //game
         val Alisa = mutableListOf<Int>()
         val Bob = mutableListOf<Int>()
+        val Job =  mutableListOf<Int>()
         val commonPull = mutableListOf<Int>()
-        val peopleCount = 2
+        val peopleCount = 3
         val handsCardCount = 2
 
         //пудлл кард хранится на сервере игроки могут отсылать полученные карты
 
         commonPull.addAll(keys)
 
-        //step1
-        var uList = keys.map { pow2(it.toLong(), cA, p.toLong()) }.shuffled()
+        var uList = emptyList<Long>()
+        //Алиса
+        repeat(1){
+            //step1
+            uList = keys.map { pow2(it.toLong(), cA, p.toLong()) }.shuffled()
 
-        repeat(handsCardCount){//step2
-            val uA = uList.shuffled().first()//боб выдаёт Алисе карту
-            uList = uList - uA
-            //Алтса расшифровывает кату
-            val cardA = pow2(uA, dA, p.toLong()).toInt()
-            Alisa.add(cardA)
-            commonPull.remove(cardA)
+            repeat(handsCardCount) {
+                //step2
+                val uA = uList.shuffled().first()//боб выдаёт Алисе карту
+                uList = uList - uA
+                //Алтса расшифровывает кату
+                val cardA = pow2(uA, dA, p.toLong()).toInt()
+                Alisa.add(cardA)
+                commonPull.remove(cardA)
+            }
         }
+        //Боб
+        repeat(1) {
+            //step3
+            uList = uList.map { pow2(it, cB, p.toLong()) }.shuffled()
 
-        //step3
-        uList = uList.map { pow2(it, cB, p.toLong()) }.shuffled()
-
-        repeat(handsCardCount){//step4
-            val uB = uList.shuffled().first()//Алтса выдаёт бобу карту
-            uList = uList - uB
-            val wB = pow2(uB, dA, p.toLong())
-            //боб расшифровывает кату
-            val cardB = pow2(wB, dB, p.toLong()).toInt()
-            Bob.add(cardB)
-            commonPull.remove(cardB)
+            repeat(handsCardCount) {
+                //step4
+                val uB = uList.shuffled().first()//Алтса выдаёт бобу карту
+                uList = uList - uB
+                val wB = pow2(uB, dA, p.toLong())
+                //боб расшифровывает кату
+                val cardB = pow2(wB, dB, p.toLong()).toInt()
+                Bob.add(cardB)
+                commonPull.remove(cardB)
+            }
         }
 
 
